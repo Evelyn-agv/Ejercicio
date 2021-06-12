83be9e5b6f889eb4bdd01f84aae81543316c2414
Evelyn-agv <85769402+Evelyn-agv@users.noreply.github.com>
2021-06-12 15:11:15 -0500

    Add files via upload

---
+++ Ejercicio/Ejercicio1.py
@@ -0,0 +1,12 @@
+n= 2.4
+
+if type(n) == int or type(n) == float:
+    print("repuesta: ", n*2)
+else:
+    print("El dato no es numerico", type(n))
+
+def mensaje(m):
+    print(m)
+mensaje("Mi primer programa")
+mensaje("Mi segundo programa")
+
